ba4e0c5d98af64634abc425421f19db61fe0bf34
Adam Joy <adamjoy@Adams-MacBook-Pro.local>
2022-05-29 14:02:18 -0500

    Edited DELIVERABLE 3

--- static/js/charts.js
+++ static/js/charts.js
@@ -126,17 +126,16 @@ function buildCharts(sample) {
 
 
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-    var metaData = data.metadata;
-    var guageArray = metadata.filter(metaObj => metaObj.id == sample);
-    
-    // 2. Create a variable that holds the first sample in the metadata array.
-    var gaugeResult = gaugeArray[0];
+    var metadata = data.metadata;
+    var gaugeArray = metadata.filter(metaObj => metaObj.id == sample);  
 
-    // 3. Create a variable that holds the washing frequency.
+    // 2. Create a variable that holds the first sample in the metadata array.
+        var gaugeResult = gaugeArray[0];
+
+    // 3. Create a variable that holds the washing frequency.  
     var wfreqs = gaugeResult.wfreq;
     console.log(wfreqs)
-   
-    
+
     // 4. Create the trace for the gauge chart.
     var gaugeData = [{
       value: wfreqs,
@@ -147,20 +146,21 @@ function buildCharts(sample) {
         axis: {range: [null,10], dtick: "2"},
 
         bar: {color: "black"},
-        steps: [
-          {range: [0,2], color: "red"},
-          {range: [2,4], color: "orange"},
-          {range: [4,6], color: "yellow"},
-          {range: [6,8], color: "lightgreen"},
-          {range: [8,10], color: "green"}
-        ], dtick: 2
+        steps:[
+          {range: [0, 2], color: "red"},
+          {range: [2, 4], color: "orange"},
+          {range: [4, 6], color: "yellow"},
+          {range: [6, 8], color: "lightgreen"},
+          {range: [8, 10], color: "green"}
+        ],
+        dtick: 2
       }
     }];
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
-      automargin: true
-     };
+     automargin: true
+    };
 
     // 6. Use Plotly to plot the gauge data and layout.
     Plotly.newPlot("gauge", gaugeData, gaugeLayout)
